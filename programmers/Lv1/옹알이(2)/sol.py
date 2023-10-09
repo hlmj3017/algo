59c3561bfe93fe89b414911455f11945b8d9febc
minj <hlmj3017@naver.com>
2023-10-09 20:38:10 +0900

    옹알이(2)

---
+++ programmers/Lv1/옹알이(2)/sol.py
@@ -0,0 +1,16 @@
+def solution(babbling):
+    answer = 0
+    can = ['aya', 'ye', 'woo', 'ma']
+    
+    for bab in babbling:  # babbling의 단어 하나씩 확인
+        for c in can:
+            if c * 2 not in bab:  # 연속으로 나오지 않으면 공백(' ')으로 대체
+                bab = bab.replace(c, ' ')
+                
+        if bab.isspace():  # 모든 발음 조합이 공백으로 대체되었으면 (발음 가능한 단어일 경우)
+            answer += 1    # answer를 1 증가
+            
+    return answer
+
+print(solution(["aya", "yee", "u", "maa"]))
+print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
