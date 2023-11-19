282cf015e5d126415204a405d12491224019844a
minj <hlmj3017@naver.com>
2023-11-19 16:13:16 +0900

    중복제거학기

---
+++ programmers_sql/동물수구하기/sol.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+-- 테이블 전체 행의 숫자를 출력한다
+SELECT COUNT(*) AS COUNT
+FROM ANIMAL_INS;
---
+++ programmers_sql/중복제거하기/sol.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+-- DISTINCT : 중복을 없애주지만 정렬을 해주지 않는다.
+-- GROUP BY : 중복을 없애주고 정렬도 해준다.
+SELECT COUNT(DISTINCT(NAME)) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME != "NULL";
+
+-- 다른 풀이
+SELECT COUNT(*) AS COUNT
+FROM (
+    SELECT NAME
+    FROM ANIMAL_INS
+    WHERE NAME IS NOT NULL
+    GROUP BY NAME
+) AS A;
